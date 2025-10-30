99899ff8f00f4195f0314f33e664463b7a899461
GovindVerma037 <ahirwargovind037@gmail.com>
2025-10-30 20:26:27 +0530

    my first commit

---
+++ day1/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+ function App() {
+
+
+  return (
+      <>
+     <h1>App Ke andar se hoga sara kam</h1>
+      </>
+  )
+
+}
+export default App;
+
---
+++ seriyans01/src/App.jsx
@@ -0,0 +1,8 @@
+function App(){
+  return(
+      <> 
+       <h1> Hii Govind  !!!!</h1>
+      </>
+  )
+}
+export default App;
